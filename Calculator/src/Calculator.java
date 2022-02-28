6d8a598f0eda4343504186bc2701cdd12013d62f
MaxRaitin <jhosh1212@gmail.com>
2022-02-28 16:54:42 +0700

    2nd commit

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -5,22 +5,24 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> y == 0 ? division(x, y) : x / y;
+    IntBinaryOperator division = (x, y) -> x / y;
+
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
     Consumer<Integer> println = System.out::println;
 
-    private static Integer division(Integer x, Integer y) {
-        int answer = 0;
+    void apply(IntBinaryOperator op, int x, int y) {
         try {
-            answer = x / y;
-        } catch (ArithmeticException e) {
-            System.out.println("Zero division");
+            int result = op.applyAsInt(x, y);
+            System.out.println("Result is " + result);
         }
-        return answer;
+        catch(ArithmeticException e) {
+            System.err.println("Division by zero!");
+        }
     }
 }
+
 
 
 
--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,16 +1,11 @@
-import java.util.function.IntBinaryOperator;
-
 public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(2, 1);
-        int c = calc.divide.apply(4, 0);
-//        int q = calc.division.applyAsInt(6,0);
-        //надо "поймать" ArithmeticException, чтобы код работал
-        int d = calc.abs.apply(-5);
-        calc.println.accept(c);
-        calc.println.accept(d);
-
+        calc.apply(calc.division, 1_337, 4);
+        calc.apply(calc.division, 1_337, 0);
+        calc.println.accept(b);
+        calc.println.accept(a);
     }
 }
