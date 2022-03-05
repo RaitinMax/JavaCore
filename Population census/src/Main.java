a1c6875c8681b53a6052f23e7fe0e16d9c124d6e
MaxRaitin <jhosh1212@gmail.com>
2022-03-05 11:37:08 +0700

    5 commit

--- Population census/src/Main.java
+++ Population census/src/Main.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        countNonAdult(persons);
+        militaryList(persons);
+
+    }
+
+    public static void countNonAdult(Collection<Person> person) {
+        long count = person.stream()
+                .filter(persons -> persons.getAge() < 18)
+                .count();
+        System.out.println(count);
+    }
+
+    public static void militaryList(Collection<Person> person) {
+        List<String> collect = person.stream()
+                .filter(persons -> persons.getSex().equals(Sex.MAN))
+                .filter(persons -> persons.getAge() > 18 && persons.getAge() < 27)
+                .map(name -> name.getName()).collect(Collectors.toList());
+        System.out.println(collect);
+    }
 }
--- Working with numbers/src/Main.java
+++ Working with numbers/src/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        list.stream()
+                .filter(number->number>0)
+                .filter(number->number%2==0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(result -> System.out.print(result+" "));
+    }
 }
